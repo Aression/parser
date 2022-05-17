ba3080e831ac2f0865a2e3575213adb1fbcf73c5
Aression <1040901628@qq.com>
2022-05-17 19:36:15 +0800

    other files

---
+++ cmake-build-debug/CMakeFiles/parser.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/CPPWorkDIR/parser/file_reader.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/file_reader.cpp.obj"
+  "D:/CPPWorkDIR/parser/lexer.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/lexer.cpp.obj"
+  "D:/CPPWorkDIR/parser/main.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/main.cpp.obj"
+  "D:/CPPWorkDIR/parser/parser.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/parser.cpp.obj"
+  "D:/CPPWorkDIR/parser/reminder.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/reminder.cpp.obj"
+  "D:/CPPWorkDIR/parser/token.cpp" "D:/CPPWorkDIR/parser/cmake-build-debug/CMakeFiles/parser.dir/token.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/file_reader.cpp.obj"
+  "CMakeFiles/parser.dir/lexer.cpp.obj"
+  "CMakeFiles/parser.dir/main.cpp.obj"
+  "CMakeFiles/parser.dir/parser.cpp.obj"
+  "CMakeFiles/parser.dir/reminder.cpp.obj"
+  "CMakeFiles/parser.dir/token.cpp.obj"
+  "libparser.dll.a"
+  "parser.exe"
+  "parser.exe.manifest"
+  "parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
